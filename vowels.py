732803eb74ebc5cd225bc37ae70b91fbaafdcffb
Anusha-hub1 <anushatech1996@gmail.com>
2025-08-06 12:32:40 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,7 @@
+# Input any alphabet and check whether it is vowel or consonant
+
+char = input("Enter the character: ")
+if (char=="a" or char=="e" or char=="i" or char=="o" or char=="u"):
+    print(char,"Enter the char is vowel")
+else:
+    print(char, "Enter the char is consonant")
