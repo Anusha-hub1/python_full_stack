5cbfec92db608175254472aec3a83045b8ef9cc8
Anusha-hub1 <anushatech1996@gmail.com>
2025-08-06 12:35:01 +0530

    Add files via upload

---
+++ zero.py
@@ -0,0 +1,11 @@
+#Check whether given number is positive, negative or zero
+
+user_input=input("Enter the num: ")
+a=int(user_input)
+if a<0:
+  print("Num is -ve")
+elif a>0:
+   print("Num is +ve")
+else:
+   print("zero")
+
